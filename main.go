faeb22018b1702c7bbb20bd8c1ad6cec0c89fa46
lempzz <l3mpzz@gmail.com>
2023-01-27 15:09:15 +0200

    use queries

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"time"
 
 	"github.com/lempzz/go-sqlc-learning/components"
 	"github.com/lempzz/go-sqlc-learning/storage/dbs"
@@ -17,6 +19,20 @@ func main() {
 	queries = dbs.New(db)
 	
 	newAccount("John Doe", 222333)
+
+	acc, err := queries.AccountGet(context.Background(), 1); if err == nil {
+		fmt.Printf("account: %v \n", acc)
+	}
+
+	queries.NewDeal(context.Background(), dbs.NewDealParams{
+		AccountID: acc.ID,
+		Type: 0,
+		OpenPrice: "1.2",
+		ClosePrice: "1.3",
+		OpenTime: time.Date(2023, time.January, 25, 15, 0, 0, 0, time.UTC),
+		CloseTime: time.Now().UTC(),
+		Profit: "123.56",
+	})
 }
 
 func newAccount(name string, num int64) {
